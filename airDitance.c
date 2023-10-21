fd1bae2f999426a8a3627361d873d4f0426d55ae
syeda khoula abid <syeda.khoula.abid234@gmail.com>
2023-10-22 03:54:58 +0500

    initializing value

---
+++ airDitance.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+
+//submitted by: syeda khola abid
+//reg #: SP23-BSE-078
+
+int main(void)
+{
+    double LA1,LA2,LO1,LO2;
+    const float avg =6371.0;
+    const float PI= 3.1415;
+    printf("Enter the Longitude and Latitude of origin in Degree \n");
+    scanf("%f %f",&LA1 , &LA2);
+    
+}
